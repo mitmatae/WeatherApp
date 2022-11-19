ac941ef4b41d11ab36ef0a4d8fafd545a6597eb3
Алина Матюха <matyukha.142527@gmail.com>
2022-11-19 16:57:26 +0700

    Initial Commit

---
+++ WaetherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaetherApp
+//
+//  Created by Алина Матюха on 19.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
